a851a82d21e14d84f9038424139815d26ce34bd1
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-03-10 20:04:27 -0500

    Created wrapper for message to be sent

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/Message.java
@@ -0,0 +1,55 @@
+package edu.buffalo.cse.cse486586.groupmessenger2;
+
+/**
+ * Created by aneesh on 3/10/17.
+ */
+
+public class Message {
+    public String message;
+    public long senderID;
+    public long messageID;
+    public float priority;
+    public boolean proposed;
+    public boolean accepted;
+
+    public Message(String message, long senderID, long messageID, float priority, boolean proposed, boolean accepted) {
+        this.message = message;
+        this.senderID = senderID;
+        this.messageID = messageID;
+        this.priority = priority;
+        this.proposed = proposed;
+        this.accepted = accepted;
+    }
+
+    public Message(String message, long senderID, long messageID) {
+        this.message = message;
+        this.senderID = senderID;
+        this.messageID = messageID;
+        this.priority = -1;
+        this.proposed = false;
+        this.accepted = false;
+    }
+
+    public void setProposed(float priority) {
+        this.priority = priority;
+        this.proposed = true;
+        this.accepted = false;
+    }
+
+    public void setAccepted(float priority) {
+        this.priority = priority;
+        this.accepted = true;
+        this.proposed = false;
+    }
+
+    public int getMessageType() {
+        if (this.accepted == false && this.proposed == false) {
+            return 0; //Initial message sent over network
+        } else if (this.accepted == false && this.proposed == true) {
+            return 1; //Priority proposed and message sent over network
+        } else if (this.accepted == true && this.proposed == false) {
+            return 2; //Accepted priority and message sent finally by sender along with priority
+        }
+        return -1; //Invalid message type encountered as both can't be true
+    }
+}
